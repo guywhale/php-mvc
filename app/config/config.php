f2e16cd412e9337e7fbca7ce9a29059ea0a5aed9
Guy Whale <guywhale@gmail.com>
2022-10-28 13:04:27 +0100

    Create some useful constants in the config files. Variables will be used throughout app so easy to change in one place

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL Root
+define('URLROOT', 'http://php-mvc.lndo.site/');
+// Site Name
+define('SITENAME', 'HwaleMVC');
+// Path to views directory
+define('VIEWSPATH', '../app/views/');
+// Path to models directory
+define('MODELSPATH', '../app/views/');
