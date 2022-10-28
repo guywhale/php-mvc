d8b2d2cf8747ef2875a2c191f23ed976266fd038
Guy Whale <guywhale@gmail.com>
2022-10-28 13:16:46 +0100

    Replace list of library requires with autoloader function. Means files added into /libraries/ will automatically loaded as long as file name matches class name

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,7 +3,7 @@
     // Load Config
     require_once 'config/config.php';
 
-    // Load Libraries
-    require_once 'libraries/Core.php';
-    require_once 'libraries/Controller.php';
-    require_once 'libraries/Database.php';
+    // Autoload Core Libraries
+    spl_autoload_register(function ($className) {
+        require_once 'libraries/' . $className . '.php';
+    });
