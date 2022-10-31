d8e64c42385aaa71d34d5aa5eac445475936cbba
Guy Whale <guywhale@gmail.com>
2022-10-31 20:26:03 +0000

    Add Users class and create register method for Register page

---
+++ app/controllers/Users.php
@@ -0,0 +1,32 @@
+<?php
+
+class Users extends Controller
+{
+    public function __construct()
+    {
+
+    }
+
+    public function register()
+    {
+        // Check for POST (form submission)
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            //Process form
+        } else {
+            // Init data
+            $data = [
+                'name' => '',
+                'email' => '',
+                'password' => '',
+                'confirmPassword' => '',
+                'nameErr' => '',
+                'emailErr' => '',
+                'passwordErr' => '',
+                'confirmPasswordErr' => '',
+            ];
+
+            // Load view
+            $this->view('users/register', $data);
+        }
+    }
+}
