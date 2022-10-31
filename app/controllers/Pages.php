359a3c8958145fc11b5da962a92c39f87cc1b2b0
Guy Whale <guywhale@gmail.com>
2022-10-31 14:01:08 +0000

    Set up navbar and hero header

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -7,13 +7,23 @@ class Pages extends Controller
 {
     public function __construct()
     {
-
     }
 
     public function index()
     {
         $data = [
             'title' => 'Hwale Shareposts',
+            'description' => 'Simple social network built on the Hwale PHP framework.'
+        ];
+
+        $this->view('pages/index', $data);
+    }
+
+    public function about()
+    {
+        $data = [
+            'title' => 'About',
+            'description' => 'App to share posts with other users.'
         ];
 
         $this->view('pages/index', $data);
--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -10,5 +10,6 @@
     <title><?= SITENAME; ?></title>
 </head>
 <body>
+    <?php require APPROOT . '/views/inc/navbar.php'; ?>
     <div class="container">
 
