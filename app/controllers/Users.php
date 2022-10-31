acad60aea30a4005b0646ac76a76cdc30c1319d4
Guy Whale <guywhale@gmail.com>
2022-10-31 20:57:45 +0000

    Set up login form and standardise button styles on both registration and login form

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -29,4 +29,23 @@ class Users extends Controller
             $this->view('users/register', $data);
         }
     }
+
+    public function login()
+    {
+        // Check for POST (form submission)
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            //Process form
+        } else {
+            // Init data
+            $data = [
+                'email' => '',
+                'password' => '',
+                'emailErr' => '',
+                'passwordErr' => '',
+            ];
+
+            // Load view
+            $this->view('users/login', $data);
+        }
+    }
 }
--- app/views/users/login.php
+++ app/views/users/login.php
@@ -1,13 +1,9 @@
 <?php
 [
-    'name' => $name,
     'email' => $email,
     'password' => $passwword,
-    'confirmPassword' => $confirmPassword,
-    'nameErr' => $nameErr,
     'emailErr' => $emailErr,
     'passwordErr' => $passwordErr,
-    'confirmPasswordErr' => $confirmPasswordErr,
 ] = $data;
 
 ?>
@@ -16,20 +12,9 @@
 <div class="row">
     <div class="col-md-8 col-lg-6 mx-auto">
         <div class="card card-body bg-light mt-5">
-            <h2 class="mb-3">Create an account</h2>
-            <p>Please fill out this form to register with us.</p>
+            <h2 class="mb-3">Login</h2>
+            <p>Please fill in your credentials to login.</p>
             <form action="<?= URLROOT; ?>/users/register" method="POST">
-                <div class="form-group mb-3">
-                    <label for="name">Name: <sup>*</sup></label>
-                    <input type="text"
-                        name="name"
-                        class="form-control form-control-lg <?= !empty($nameErr) ? 'is-invalid' : '' ?>"
-                        value="<?= $name; ?>"
-                    >
-                    <?php if ($nameErr) { ?>
-                        <span class="invalid-feedback"><?= $nameErr; ?></span>
-                    <?php } ?>
-                </div>
                 <div class="form-group mb-3">
                     <label for="email">Email: <sup>*</sup></label>
                     <input type="email"
@@ -52,24 +37,13 @@
                         <span class="invalid-feedback"><?= $passwordErr; ?></span>
                     <?php } ?>
                 </div>
-                <div class="form-group mb-4">
-                    <label for="confirmPassword">Confirm Password: <sup>*</sup></label>
-                    <input type="password"
-                        name="confirmPassword"
-                        class="form-control form-control-lg <?= !empty($confirmPasswordErr) ? 'is-invalid' : '' ?>"
-                        value=""
-                    >
-                    <?php if ($confirmPasswordErr) { ?>
-                        <span class="invalid-feedback"><?= $confirmPasswordErr; ?></span>
-                    <?php } ?>
-                </div>
                 <div class="row">
                     <div class="col">
-                        <input type="submit" value="Register" class="btn btn-success d-block w-100">
+                        <input type="submit" value="Login" class="btn btn-success d-block w-100">
                     </div>
                     <div class="col">
-                        <a href="<?= URLROOT; ?>/users/login" class="btn btn-light d-block">
-                            Have an account?
+                        <a href="<?= URLROOT; ?>/users/register" class="btn btn-light d-block">
+                            Register an account
                         </a>
                     </div>
                 </div>
