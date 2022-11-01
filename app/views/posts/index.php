f2d07f9546762bd186b1ff3fd15a1375579ea188
Guy Whale <guywhale@gmail.com>
2022-11-01 14:24:43 +0000

    Set up posts controller and view

---
+++ app/views/posts/index.php
@@ -0,0 +1,13 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+<div class="row">
+    <div class="col-md-6">
+        <h1>Posts</h1>
+    </div>
+    <div class="col-md-6 text-end">
+        <a href="<?= URLROOT; ?>/posts/add" class="btn btn-primary ms-auto">
+            <i class="fa fa-pencil"></i> Add post
+        </a>
+    </div>
+</div>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
+
