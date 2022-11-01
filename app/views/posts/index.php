919d7cfe9cb4265afec5b5ceff73b7c60186ab79
Guy Whale <guywhale@gmail.com>
2022-11-01 14:48:30 +0000

    Set up Posts model for getting all posts and use it to populate controller and view for posts page

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -1,5 +1,13 @@
+<?php
+
+[
+    'posts' => $posts
+] = $data;
+
+?>
+
 <?php require APPROOT . '/views/inc/header.php'; ?>
-<div class="row">
+<div class="row mb-3">
     <div class="col-md-6">
         <h1>Posts</h1>
     </div>
@@ -9,5 +17,23 @@
         </a>
     </div>
 </div>
+
+<?php foreach ($posts as $post) { ?>
+    <div class="card card-body mb-3">
+        <h4 class="card-title">
+            <?= $post->title; ?>
+        </h4>
+        <div class="bg-light p-2 mb-3">
+            Written by <?= $post->name; ?> on <?= $post->postCreated ?>
+        </div>
+        <p class="card-text">
+            <?= $post->body; ?>
+        </p>
+        <a href="<?= URLROOT; ?>/posts/show/<?= $post->postId; ?>" class="btn btn-dark">
+            More
+        </a>
+    </div>
+<?php } ?>
+
 <?php require APPROOT . '/views/inc/footer.php'; ?>
 
