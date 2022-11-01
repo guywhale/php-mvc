3b245a38ecabd2d2025ae4860afea1a7c5884b3a
Guy Whale <guywhale@gmail.com>
2022-11-01 17:33:22 +0000

    Add ability to edit existing posts if post was created by user and user is logged in

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -90,6 +90,71 @@ class Posts extends Controller
         }
     }
 
+    public function edit($id)
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            //Process form
+            $title = $body = '';
+
+            // Sanitize POST array
+            $title = trim(htmlspecialchars($_POST['title']));
+            $body = trim(htmlspecialchars($_POST['body']));
+
+            // Init data
+            $data = [
+                'id' => $id,
+                'title' => $title,
+                'body' => $body,
+                'userId' => $_SESSION['userId'],
+                'titleErr' => '',
+                'bodyErr' => '',
+            ];
+
+            // Vaidate Title
+            if (empty($data['title'])) {
+                $data['titleErr'] = 'Please enter a title.';
+            }
+
+            // Vaidate Body
+            if (empty($data['body'])) {
+                $data['bodyErr'] = 'Please enter some body text.';
+            }
+
+            // Make sure there's no errors
+            if (empty($data['titleErr']) && empty($data['bodyErr'])) {
+                // Validated
+                if ($this->postModel->updatePost($data)) {
+                    flash('postMessage', 'Post updated.');
+                    redirect('posts');
+                } else {
+                    die('Something went wrong.');
+                }
+            } else {
+                // Load view with errors
+                $this->view('posts/edit', $data);
+            }
+        } else {
+            // Get current post from model
+            $post = $this->postModel->getPostById($id);
+
+            // Check for owner to ensure non-logged in users can't access URL
+            if ($post->userId !== $_SESSION['userId']) {
+                // redirect back to posts page if not logged in.
+                redirect('posts');
+            }
+
+            $data = [
+                'id' => $id,
+                'title' => $post->title,
+                'body' => $post->body,
+                'titleErr' => '',
+                'bodyErr' => '',
+            ];
+
+            $this->view('posts/edit', $data);
+        }
+    }
+
     // Show individual posts
     public function show($id)
     {
