7b893041ce92a46d173f6e9ed9a9837860596672
Guy Whale <guywhale@gmail.com>
2023-09-21 18:07:13 +0100

    Remove spaces

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,13 +1,13 @@
 <?php
 
-    // Load DotEnv Class first to populate config.php
-    require_once 'libraries/DotEnv.php';
+// Load DotEnv Class first to populate config.php
+require_once 'libraries/DotEnv.php';
 
 
-    // Load Config
-    require_once 'config/config.php';
+// Load Config
+require_once 'config/config.php';
 
-    // Autoload Core Libraries
-    spl_autoload_register(function ($className) {
-        require_once 'libraries/' . $className . '.php';
-    });
+// Autoload Core Libraries
+spl_autoload_register(function ($className) {
+    require_once 'libraries/' . $className . '.php';
+});
